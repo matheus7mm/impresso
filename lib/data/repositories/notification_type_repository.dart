1254661daec314d28a7b509744c6266a252f89ad
Matheus Moraes <matheus7mm@hotmail.com>
2022-03-13 15:07:11 -0300

    Adding exception handling in the repository

--- lib/data/repositories/notification_type_repository.dart
+++ lib/data/repositories/notification_type_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import './../../domain/domain.dart';
 
 class NotificationTypeRepository {
@@ -142,56 +144,68 @@ class NotificationTypeRepository {
     required int notificationId,
     required bool hasBeenRead,
   }) {
-    NotificationTypeEntity notificationType = notificationTypeList
-        .where((element) => element.id == notificationTypeId)
-        .first;
+    try {
+      NotificationTypeEntity notificationType = notificationTypeList
+          .where((element) => element.id == notificationTypeId)
+          .first;
 
-    NotificationEntity notification = notificationType.notifications
-        .where((element) => element.id == notificationId)
-        .first;
+      NotificationEntity notification = notificationType.notifications
+          .where((element) => element.id == notificationId)
+          .first;
 
-    final indexNotificationType =
-        notificationTypeList.indexOf(notificationType);
-    final indexNotification = notificationTypeList[indexNotificationType]
-        .notifications
-        .indexOf(notification);
+      final indexNotificationType =
+          notificationTypeList.indexOf(notificationType);
+      final indexNotification = notificationTypeList[indexNotificationType]
+          .notifications
+          .indexOf(notification);
 
-    notificationTypeList[indexNotificationType]
-            .notifications[indexNotification] =
-        notification.copyWith(hasBeenRead: hasBeenRead);
+      notificationTypeList[indexNotificationType]
+              .notifications[indexNotification] =
+          notification.copyWith(hasBeenRead: hasBeenRead);
+    } catch (error) {
+      log(error.toString());
+    }
   }
 
   void updateUnreadNotificationsCount({
     required int notificationTypeId,
   }) {
-    NotificationTypeEntity notificationType = notificationTypeList
-        .where((element) => element.id == notificationTypeId)
-        .first;
+    try {
+      NotificationTypeEntity notificationType = notificationTypeList
+          .where((element) => element.id == notificationTypeId)
+          .first;
 
-    int count = 0;
+      int count = 0;
 
-    for (final element in notificationType.notifications) {
-      if (element.hasBeenRead == false) {
-        count++;
+      for (final element in notificationType.notifications) {
+        if (element.hasBeenRead == false) {
+          count++;
+        }
       }
+
+      final index = notificationTypeList.indexOf(notificationType);
+
+      notificationTypeList[index] = notificationType.copyWith(
+        unreadNotificationsCount: count,
+      );
+    } catch (error) {
+      log(error.toString());
     }
-
-    final index = notificationTypeList.indexOf(notificationType);
-
-    notificationTypeList[index] = notificationType.copyWith(
-      unreadNotificationsCount: count,
-    );
   }
 
   void setNotificationTypeOnTop({
     required int notificationTypeId,
   }) {
-    NotificationTypeEntity notificationType = notificationTypeList.firstWhere(
-        (notificationType) => notificationType.id == notificationTypeId);
+    try {
+      NotificationTypeEntity notificationType = notificationTypeList.firstWhere(
+          (notificationType) => notificationType.id == notificationTypeId);
 
-    final index = notificationTypeList.indexOf(notificationType);
+      final index = notificationTypeList.indexOf(notificationType);
 
-    notificationTypeList.removeAt(index);
-    notificationTypeList.insert(0, notificationType);
+      notificationTypeList.removeAt(index);
+      notificationTypeList.insert(0, notificationType);
+    } catch (error) {
+      log(error.toString());
+    }
   }
 }
