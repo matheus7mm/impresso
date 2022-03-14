62524f393a2d7223b700569210b713cec68d277d
Matheus Moraes <matheus7mm@hotmail.com>
2022-03-14 08:30:50 -0300

    Removing unused assets

--- lib/ui/theme/assets/notifications_assets.dart
+++ lib/ui/theme/assets/notifications_assets.dart
@@ -1,5 +1,5 @@
 class NotificationsAssets {
-  static const String logoSvg = 'assets/logo3.svg';
+  static const String logoSvg = 'assets/logo.svg';
   static const String checkSvg = 'assets/check.svg';
   static const String s1Svg = 'assets/s1.svg';
 }
